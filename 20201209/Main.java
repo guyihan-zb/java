a02700bd10f55813b4b9e64b56bc6905f4d6a5c2
ZhangBo <2092332348@qq.com>
2021-01-20 22:53:48 +0800

    图书管理系统主函数

---
+++ 20201209/Main.java
@@ -0,0 +1,36 @@
+import book.BookList;
+import user.AdminUser;
+import user.NormalUser;
+import user.User;
+
+import java.util.Scanner;
+
+/**
+ * [description]
+ *
+ * @author： zhangbo
+ * @date: 2021-01-18
+ * @time: 09:43
+ */
+public class Main {
+    public static User login() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("请输入你的姓名：");
+        String name = scanner.nextLine();
+        System.out.println("请输入你的身份：1-》管理员 0：普通用户");
+        int choice = scanner.nextInt();
+        if(choice == 1) {
+            return new AdminUser(name);
+        }else {
+            return new NormalUser(name);
+        }
+    }
+    public static void main(String[] args) {
+        BookList bookList=new BookList();
+        User user=login();
+        while(true){
+            int choice=user.menu();
+            user.doOperation(choice, bookList);
+        }
+    }
+}
